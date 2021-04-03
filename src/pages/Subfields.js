e64d74d5faa0e6545e0d529f663659edfc2984e6
Ronald Velez <ronald.velez@upr.edu>
2021-04-02 21:41:31 -0400

    Added subfield table prototype.

--- src/pages/Subfields.js
+++ src/pages/Subfields.js
@@ -1,6 +1,68 @@
-import React from 'react';
-import '../App.css';
+import React, {useMemo} from 'react'
+import './Subfields.css'
+import {useTable} from 'react-table'
+import DATA from '../table/DATA.json'
+import {COLUMNS} from '../table/Columns'
 
-export default function Subfields() {
-    return <h1 className='subfields'>Subfields</h1>;
+function Subfields(){
+    const columns = useMemo(() => COLUMNS, []);
+    const data = useMemo(() => DATA, []);
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        footerGroups,
+        rows,
+        prepareRow
+    } = useTable({
+        columns,
+        data
+    })
+
+    return (
+        <form>
+            <div className="Subfields">
+                <h1>Subfields</h1>
+                <p>This a table listing all subfields</p>
+                <div className="SubfieldsForm">
+                    <>
+                        <table {...getTableProps()}>
+                            <thead>
+                            {headerGroups.map(headerGroup => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map(column => (
+                                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                    ))}
+                                </tr>
+                            ))}
+                            </thead>
+                            <tbody {...getTableBodyProps()}>
+                            {rows.map(row => {
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map(cell => {
+                                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                        })}
+                                    </tr>
+                                )
+                            })}
+                            </tbody>
+                            <tfoot>
+                            {footerGroups.map(footerGroup => (
+                                <tr {...footerGroup.getFooterGroupProps()}>
+                                    {footerGroup.headers.map(column => (
+                                        <td {...column.getFooterProps()}>{column.render('Footer')}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                            </tfoot>
+                        </table>
+                    </>
+                </div>
+            </div>
+        </form>
+    )
 }
+export default Subfields;
