dc384e3d13116634b1b50c1eae1d207a0ce73d0a
Linkman21 <omar.yusuf@upr.edu>
2021-04-28 00:16:53 -0400

    Survey questions added

--- src/pages/StudentSurvey.js
+++ src/pages/StudentSurvey.js
@@ -118,91 +118,74 @@ class StudentSurvey extends Component {
                 <form onSubmit={this.surveySubmit}>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans1" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans1" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans1" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans1" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Which of the following would you like to develop in the future?</label><br/>
+                        <input type="radio"name="ans1" value="Sports" onChange={this.answerSelected} /> Hardware
+                        <input type="radio"name="ans1" value="Tech" onChange={this.answerSelected} /> Software
+                        <input type="radio"name="ans1" value="CIIC" onChange={this.answerSelected} /> Both A and B (Every subfield)
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans2" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans2" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans2" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans2" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Do you understand and like math even at its most complex?</label><br/>
+                        <input type="radio"name="ans2" value="Sports" onChange={this.answerSelected} /> I agree
+                        <input type="radio"name="ans2" value="Tech" onChange={this.answerSelected} /> I only like simple math
+                        <input type="radio"name="ans2" value="CIIC" onChange={this.answerSelected} /> I do not like math
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans3" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans3" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans3" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans3" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Would you prefer to prepare visuals, special effects or texturing graphic models rather than code?</label><br/>
+                        <input type="radio"name="ans3" value="Sports" onChange={this.answerSelected} /> I agree
+                        <input type="radio"name="ans3" value="Tech" onChange={this.answerSelected} /> I do not agree
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans4" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans4" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans4" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans4" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>In a job you want to work with topics that can mix computer science with other science fields together rather than just computer science?</label><br/>
+                        <input type="radio"name="ans4" value="Sports" onChange={this.answerSelected} /> I agree
+                        <input type="radio"name="ans4" value="Tech" onChange={this.answerSelected} /> I do not agree
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans5" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans5" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans5" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans5" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Are you interested in creating robots and the development of other programs and devices that facilitate humanity’s daily life?</label><br/>
+                        <input type="radio"name="ans5" value="Sports" onChange={this.answerSelected} /> Yes
+                        <input type="radio"name="ans5" value="Tech" onChange={this.answerSelected} /> No
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans6" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans6" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans6" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans6" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>When developing software, do you prefer to prepare and manage what outcomes, impacts or requirements it may cause or need rather than coding and building it?</label><br/>
+                        <input type="radio"name="ans6" value="Sports" onChange={this.answerSelected} /> I agree
+                        <input type="radio"name="ans6" value="Tech" onChange={this.answerSelected} /> I do not agree
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans7" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans7" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans7" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans7" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Would you like to further develop existing operating systems, programming languages or creating your very own of these for specific platforms?</label><br/>
+                        <input type="radio"name="ans7" value="Sports" onChange={this.answerSelected} /> Yes
+                        <input type="radio"name="ans7" value="Tech" onChange={this.answerSelected} /> No
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans8" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans8" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans8" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans8" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Are you interested in protecting databases, financial accounts and everything that gets threatened by malicious software or hackers?</label><br/>
+                        <input type="radio"name="ans8" value="Sports" onChange={this.answerSelected} /> Yes
+                        <input type="radio"name="ans8" value="none" onChange={this.answerSelected} /> No
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans9" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans9" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans9" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans9" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Do you want to create servers to manage and store information or allow communication between a set of computers?</label><br/>
+                        <input type="radio"name="ans9" value="Sports" onChange={this.answerSelected} /> Yes
+                        <input type="radio"name="ans9" value="Tech" onChange={this.answerSelected} /> No
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans10" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans10" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans10" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans1" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Are you interested in the development of devices or systems that learn and make decisions of their own?</label><br/>
+                        <input type="radio"name="ans10" value="Sports" onChange={this.answerSelected} /> Yes
+                        <input type="radio"name="ans10" value="Tech" onChange={this.answerSelected} /> No
                     </div>
 
                     <div className="card">
-                        <label>What do you like the most?</label><br/>
-                        <input type="radio"name="ans11" value="Sports" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans11" value="Tech" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans11" value="CIIC" onChange={this.answerSelected} /> Sports
-                        <input type="radio"name="ans11" value="UPRM" onChange={this.answerSelected} /> Sports
+                        <label>Select which of the following may describe you most:</label><br/>
+                        <input type="radio"name="ans11" value="Sports" onChange={this.answerSelected} /> I want to be able to create video games, cartoons and everything that involves drawing and graphics
+                        <input type="radio"name="ans11" value="Tech" onChange={this.answerSelected} /> I want to help build robots and systems that will allow a better advanced society
+                        <input type="radio"name="ans11" value="CIIC" onChange={this.answerSelected} /> I want to form part of the virtual security workforce
+                        <input type="radio"name="ans11" value="UPRM" onChange={this.answerSelected} /> I want to understand the fundamentals needed in order to make an impactful application and how it can be refined to provide ease of use to our users
+                        <input type="radio"name="ans11" value="none" onChange={this.answerSelected} /> None of the above
                     </div>
                     <input className="feedback-button" type="submit" value="submit"/>
                 </form>
